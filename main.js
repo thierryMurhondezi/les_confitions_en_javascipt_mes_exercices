8372d5c31fc7082d88efb3686fa928affd715f68
Thierry Murhondezi Bisimwa <thierrymurhondezi@gmail.com>
2023-08-09 07:04:03 +0200

    feat: Ii am trying different cases of application of the logical condition operators

--- main.js
+++ main.js
@@ -22,3 +22,68 @@ if (comprVar==false){
     console.log("30 est >= 20")
 }
 
+//  qu'est ce qui arive , si , au lieux de l'egalive de valeurs seulement "=="  dans les parametre de notres conditions on les remplacais par l'egalité strict , 
+//  interpretation, qaund on utilise l'egalité stricte "===" on fait l'agalité des valeurs intresiques des variables , et meme les noms sens et le nom des variables , 
+
+if (comprVar===false){
+    console.log("30 est <= 20")
+}else{
+    console.log("30 est >= 20")
+}
+
+// constat , c'est la deuxieme valeur qui passe 
+//  alors , si on inverse de meme 
+
+if (comprVar===true){
+    console.log("30 est <= 20")
+}else{
+    console.log("30 est >= 20")
+}
+
+
+// comparateur logiques && === et 
+
+let a= 5;
+let b= 10;
+let c= 30;
+
+let comparVar= a>=b && c<a ;
+console.log(comparVar);
+
+let cen= a && b > c;
+console.log(cen);
+
+let cenB= a && c > c && c;
+
+console.log(cenB);
+
+// but check out this one , the answer given here !! wowo !! how come , how does && work , asan addition or as what ? 
+
+let cenC= a && a + c && c;
+console.log(cenC);
+
+let cenV= a && a;
+console.log(cenV);
+
+let cenS=a && b;
+console.log(cenS);
+
+let cenT= a||a;
+console.log(cenT);
+
+
+let cenY= a||b;
+console.log(cenY);
+
+
+
+let cenU= b||a;
+console.log(cenU);
+
+let cenI= b && a;
+console.log(cenI);
+
+
+
+let cenO= a && b;
+console.log(cenO);
